7e88f182367c233532a7d88423a20e16c9469349
Claudio-Olivera <claudioolivera753@gmail.com>
2023-03-26 13:51:31 -0300

    Add Maven

--- TpIntegrador/src/main/java/org/TpIntegrador/Ronda.java
+++ TpIntegrador/src/main/java/org/TpIntegrador/Ronda.java
@@ -39,60 +39,6 @@ public class Ronda {
 //    }
 
 
-//    public static void compararPartidosYPronosticos(List<String> partidos, List<String> pronosticos) {
-//        Map<String, Integer> puntosPorPersona = new HashMap<>(); // Map para almacenar los puntos de cada persona
-//        List<String> personasPronosticadoras = new ArrayList<>(); // Lista para almacenar todas las personas que hicieron un pronóstico
-//
-//        // Iterar a través de los pronósticos y agregar cada persona a la lista de personasPronosticadoras
-//        for (String pronostico : pronosticos) {
-//            pronostico = pronostico.substring(1, pronostico.length() - 1);
-//            String[] partes = pronostico.split(", ");
-//            String personaPronosticadora = partes[0];
-//            personasPronosticadoras.add(personaPronosticadora);
-//            puntosPorPersona.put(personaPronosticadora, 0); // Agregar la persona al map con 0 puntos
-//        }
-//
-//        int indexPronostico = 0; // Índice del pronóstico actual
-//
-//        // Iterar a través de los partidos y comparar los pronósticos con los resultados reales
-//        for (String persona : personasPronosticadoras) {
-//            int puntos = puntosPorPersona.get(persona);
-            /**
-             * Hay que corregir esta logica, ya que si se trata de una persona con puntaje 0, se repite su resultado varias veces en consola. y no queremos eso.
-             */
-//            if (puntos > 0) {
-//                // Si los puntos para esta persona ya se han impreso, saltar al siguiente persona
-//                continue;
-//            }
-//            for (int i = 0; i < partidos.size(); i++) {
-//                String equipoGanador = partidos.get(i);
-//
-//                // Comprobar si el índice del pronóstico actual coincide con el índice del partido actual
-//                if (indexPronostico < pronosticos.size()) {
-//                    String pronostico = pronosticos.get(indexPronostico);
-//                    pronostico = pronostico.substring(1, pronostico.length() - 1);
-//                    String[] partes = pronostico.split(", ");
-//                    String personaPronosticadora = partes[0];
-//                    String equipoPronosticado = partes[1];
-//
-//                    if (equipoGanador.equals(equipoPronosticado)) {
-//                        puntos = puntosPorPersona.get(personaPronosticadora);
-//                        puntosPorPersona.put(personaPronosticadora, puntos + 1); // Agregar 1 punto a la persona
-//                    }
-//
-//
-//                    indexPronostico++; // Incrementar el índice del pronóstico de la persona actual si no es la misma persona
-//
-//                }
-//
-//            }
-//
-//            // Imprimir los puntos de cada persona
-//            puntos = puntosPorPersona.getOrDefault(persona, 0);
-//            System.out.print("\nPuntos ganados por " + persona + ": " + puntos);
-//                }
-//            }
-
             public static void compararPartidosYPronosticos(List<String> partidos, List<String> pronosticos) {
                 Map<String, Integer> puntosPorPersona = new HashMap<>();
                 List<String> personasPronosticadoras = new ArrayList<>();
