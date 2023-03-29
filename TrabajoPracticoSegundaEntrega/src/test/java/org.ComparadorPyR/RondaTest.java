22a3a921e707943e66ee1bc7736353a05ca01f41
Cintia Martinez <cintialujan@hotmail.com>
2023-03-29 16:50:28 -0300

    ‘mensaje’

---
+++ TrabajoPracticoSegundaEntrega/src/test/java/org.ComparadorPyR/RondaTest.java
@@ -0,0 +1,17 @@
+package org.ComparadorPyR;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RondaTest {
+
+    @Test
+       void imprimirPuntosPorPersona() {
+        Ronda ronda= new Ronda();
+       Assertions.
+    }
+}
